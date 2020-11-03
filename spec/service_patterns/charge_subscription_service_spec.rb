549e0cec01948cfeac3de577b7ffae4cdc300697
Graham Marlow <mgmarlow@gmail.com>
2020-11-03 09:46:23 -0800

    Add tests

---
+++ spec/service_patterns/charge_subscription_service_spec.rb
@@ -0,0 +1,30 @@
+RSpec.describe ServicePatterns::ChargeSubscriptionService do
+  let(:email)   { "foo@foo.com" }
+  let(:context) { ServicePatterns::ChargeSubscriptionService.call(email: email) }
+
+  describe "#call" do
+    context "valid email" do
+      let(:email) { "foo@foo.com" }
+
+      it "succeeds" do
+        expect(context).to be_a_success
+      end
+
+      it "charges the card" do
+        expect(context.amount).to eq(50)
+      end
+    end
+
+    context "invalid email" do
+      let(:email) {}
+
+      it "fails" do
+        expect(context).to be_a_failure
+      end
+
+      it "provides error message" do
+        expect(context.errors).to eq "Invalid email"
+      end
+    end
+  end
+end
